531df5c7789e927d55a75658c91edcaaa3a327d1
winter <csf178@gmail.com>
2022-11-25 18:51:30 +0800

    kmp search

--- kmp.js
+++ kmp.js
@@ -2,7 +2,7 @@ function search(source, pattern) {
     //outer is a label
     outer: for(let i = 0; i < source.length; i++) {
         for(let j = 0; j < pattern.length; j++) {
-            if(source[i] !== pattern[j]) {
+            if(source[i + j] !== pattern[j]) {
                 continue outer;
             }
         }
@@ -52,7 +52,79 @@ function search3(source, pattern) {
     return -1;
 }
 
-console.log(search3("abababxabcababcababc", "ababc"));
+//i: aabaabaaab
+//j:    aabaabaaab
+//      ^
+//n:0010
+function generateNext(pattern) {
+    let next = [0, 0];
+    let i = 1, j = 0;
+    while(i < pattern.length) {
+        if(pattern[i] === pattern[j]) {
+            next[++i] = ++j;
+        } else if(j === 0) { 
+            next[++i] = 0;
+        } else {
+            j = next[j];
+        }
+    }
+    return next;
+}
+
+function generateNext2(pattern) {
+    let next = [0, 0];
+    let j = 0;
+
+    for(let i = 1; i < pattern.length; ) {
+        if(pattern[i] === pattern[j]) {
+            next[++i] = ++j;
+
+        } else if(j === 0){
+            next[++i] = 0;
+        } else {
+            j = next[j];
+            //i--;
+        }
+    }
+    
+    return next;
+}
+
+
+console.log(generateNext2("aabaabaaab"));
+
+
+function search4(source, pattern) {
+    //outer is a label
+    let next = [0, 0];
+    let i = 1, j = 0;
+    while(i < pattern.length) {
+        if(pattern[i] === pattern[j]) {
+            next[++i] = ++j;
+        } else if(j === 0) { 
+            next[++i] = 0;
+        } else {
+            j = next[j];
+        }
+    }
+    i = 0, j = 0;
+    while(i < source.length) {
+        if(source[i] === pattern[j]) {
+            j++;
+            i++;
+            if(j === pattern.length) {
+                return i - j;
+            }
+        } else if(j !== 0) {
+            j = next[j];
+        } else {
+            i++;
+        }
+    }
+    return -1;
+};
+
+//search3("abababxabcababcababc", "ababc"));
 /*
     ababc
 abababcaabababc
